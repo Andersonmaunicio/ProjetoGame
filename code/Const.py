8c1ea982699be2eebab255f8a447b8983f75b8d1
Andersonmaunicio <andersonmaunicio@gmail.com>
2025-03-28 00:43:46 -0300

    Creating ABS Class Creating Factory Level 1 Bg implementation: parallax

--- code/Const.py
+++ code/Const.py
@@ -1,14 +1,25 @@
 # C
 COLOR_BLUE = (0, 0, 255)
 COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 0)
+
+# E
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+}
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P',
                'SCORE',
                'EXIT')
-# Y
-COLOR_YELLOW = (255, 255, 0)
+
 
 # W
 WIN_WIDTH = 576
--- code/Game.py
+++ code/Game.py
@@ -3,8 +3,8 @@
 import pygame
 
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
-from code.level import Level
-from code.menu import Menu
+from code.Level import Level
+from code.Menu import Menu
 
 
 class Game:
@@ -21,7 +21,7 @@ class Game:
                 level = Level(self.window, 'Level1', menu_return)
                 level_return = level.run()
             elif menu_return == MENU_OPTION[3]:
-                pygame.quit()  # Close Windows
+                pygame.quit()  # Close Window
                 quit()  # end pygame
             else:
                 pass
